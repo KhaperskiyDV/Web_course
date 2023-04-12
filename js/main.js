33a25dcb6ba9f607a1392d621e871730e031391b
KhaperskiyDV <72084663+KhaperskiyDV@users.noreply.github.com>
2023-04-12 09:42:04 +0700

    20230412

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const swiper = new Swiper(".mySwiper", {
-    navigation: {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: false
+  },  
+  navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     autoplay: {
-        delay: 100000,
+        delay: 5000,
         disableOnInteraction: true,
       },
   });
