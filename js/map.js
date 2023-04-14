469f82a4bbb0f9196add79f9a669e19396a94340
Хаперский Дмитрий Викторович <KHAPERSKIY152846@hq.corp.mmk.chel.su>
2023-04-14 15:12:16 +0700

    about events

---
+++ js/map.js
@@ -0,0 +1,19 @@
+let center = [54.421975805284525, 86.30455824901044];
+
+function init() {
+    let map = new ymaps.Map('map-test', {
+        center: center,
+        zoom: 18
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init);
